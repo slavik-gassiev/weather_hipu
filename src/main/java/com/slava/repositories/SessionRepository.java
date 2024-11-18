a79e0176998b24ed0d32af53f1a41bc29fe13137
slavik-gassiev <slavikgassiev1872002@gmail.com>
2024-11-18 14:04:37 +0300

    resolved mistakes and add getLocationByUser method

--- src/main/java/com/slava/repositories/SessionRepository.java
+++ src/main/java/com/slava/repositories/SessionRepository.java
@@ -2,8 +2,10 @@ package com.slava.repositories;
 
 import com.slava.entities.Session;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+@Repository
 public interface SessionRepository extends JpaRepository<Session, UUID> {
 }
--- src/main/java/com/slava/services/LocationService.java
+++ src/main/java/com/slava/services/LocationService.java
@@ -1,10 +1,13 @@
 package com.slava.services;
 
 import com.slava.entities.Location;
+import com.slava.entities.User;
 import com.slava.repositories.LocationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 public class LocationService {
@@ -18,5 +21,9 @@ public class LocationService {
     @Transactional
     public void save(Location location) {
         locationRepository.save(location);
+    }
+
+    public List<Location> getUserLocations(User user) {
+        return locationRepository.getLocationByUser(user);
     }
 }
